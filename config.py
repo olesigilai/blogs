f220399ef79a0be2d2bf43870fe28458ea66fade
olesigilai <ole_sigilai@yahoo.com>
2021-03-06 16:59:05 +0300

    config file is set and email configs done

--- config.py
+++ config.py
@@ -1,9 +1,33 @@
-from flask import Flask
-app = Flask(__name__)
+import os
 
-@app.route('/')
-def index():
-    return '<h1> GIDEON</h1>'
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
-if __name__ == '__main__':
-    app.run(debug = True)
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME =os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD =os.environ.get("MAIL_PASSWORD")
+    
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_GREEN_URL")
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:kipkirui@localhost/blogs_test'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:kipkirui@localhost/blogs'
+    
+    DEBUG = True
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}

15e6cd0f17518897e125987728b8874c9ced06af
olesigilai <ole_sigilai@yahoo.com>
2021-03-06 18:32:49 +0300

    start file for running the project is initialized

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MAIL_USERNAME='gideonkipkirui95@gmail.com'
+export MAIL_PASSWORD='34281475'
+export SECRET_KEY='kipkirui'
+export DATABASE_URL='postgresql+psycopg2://moringa:kipkirui@localhost/pitches'
+
+python3.6 manage.py server
